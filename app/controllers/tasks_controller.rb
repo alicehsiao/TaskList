77ab65baab3a6de2c0432e5bcb3f0f3e88672028
Alice Hsiao <alice.hsiao87@gmail.com>
2018-09-17 16:41:16 -0700

    Created books#show and checked that it worked

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,17 @@
-LIST = ["Continue working on Personal Website", "Read Sorting for CS Fun", "Complete Fibonacci", "Read Hotel Solution", "Log Food", "Shoulder PT: I's, Y's, T's"]
-
+LIST = [
+  { id: 1, name: "Personal Website", description: "Look into media queries to make website responsive on all devices", completion_date: "2018.09.21" },
+  { id: 2, name: "Sorting for CS Fun", description: "Read and code each sorting algorithm.", completion_date: "2018.09.18" },
+  { id: 3, name: "Hotel Solution", description: "Read and understand instructors solution to OOD task, Hotel", completion_date: "2018.09.23" },
+  { id: 4, name: "Shoulder PT", description: "Once per day, 10 reps for I's, Y's, and T's", completion_date: "2018.09.20" },
+]
 
 class TasksController < ApplicationController
   def index
     @list = LIST
+  end
+
+  def show
+    id = params[:id]
+    @task = LIST.find {|item| item[:id] == id.to_i}
   end
 end
