2500ffe5de3f6ccc10a73f5febc335bc171ae77b
Alice Hsiao <alice.hsiao87@gmail.com>
2018-09-18 14:22:34 -0700

    Changed index and show to work with a database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,15 @@
-LIST = [
-  { id: 1, name: "Personal Website", description: "Look into media queries to make website responsive on all devices", completion_date: "2018.09.21" },
-  { id: 2, name: "Sorting for CS Fun", description: "Read and code each sorting algorithm.", completion_date: "2018.09.18" },
-  { id: 3, name: "Hotel Solution", description: "Read and understand instructors solution to OOD task, Hotel", completion_date: "2018.09.23" },
-  { id: 4, name: "Shoulder PT", description: "Once per day, 10 reps for I's, Y's, and T's", completion_date: "2018.09.20" },
-]
 
 class TasksController < ApplicationController
   def index
-    @list = LIST
+    @tasks = Task.all.order(:completion_date)
   end
 
   def show
-    id = params[:id]
-    @task = LIST.find {|item| item[:id] == id.to_i}
+    id = params[:id].to_i
+    @task = Task.find_by(id: id)
+
+    if @task.nil?
+      render :notfound, status: :not_found
+    end
   end
 end
