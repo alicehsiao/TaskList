40e391a3a8e1411250c9ab80f90d62a2534e159f
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-04 10:03:08 -0700

    Deleted migrations

--- db/migrate/20180921204306_create_new_column.rb
+++
@@ -1,6 +0,0 @@
-class CreateNewColumn < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tasks, :c_date, :string
-  end
-end
-z
--- db/migrate/20180921204621_delete_c_date_column.rb
+++
@@ -1,5 +0,0 @@
-class DeleteCDateColumn < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tasks, :c_date
-  end
-end
